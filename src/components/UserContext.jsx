21d75661b1ed0f1d06b7f4b071171f068ead151a
sunny <sanyasappadu4@gmail.com>
2023-04-19 21:21:49 +0530

    modification usercontentmodule

--- src/components/UserContext.jsx
+++ src/components/UserContext.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const UserContext=React.createContext('naidu')
+const UserContext = React.createContext('naidu')
 
-const UsreProvider=UserContext.Provider
-const UsreConsumer=UserContext.Consumer
+const UsreProvider = UserContext.Provider
+const UsreConsumer = UserContext.Consumer
 
-export {UsreProvider,UsreConsumer}
+export { UsreProvider, UsreConsumer }
 export default UserContext
+
+console.log("hello todo listss")
